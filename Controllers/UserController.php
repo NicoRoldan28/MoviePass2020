d2c90b967530785381564dc3a41bc38d2b569b47
nico <nicolasroldan31@gmail.com>
2020-11-17 00:01:58 -0300

    mod phpMailer

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -12,12 +12,19 @@
     use DAO\GenderDAO as GenderDAO;
     use Models\Gender as Gender;
 
-    use PHPMailer\PHPMailer;
-    use PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-    require 'PHPMailer/Exception.php';
-    require 'PHPMailer/PHPMailer.php';
-    require 'PHPMailer/SMTP.php';
+    //require 'PHPMailer/Exception.php';
+    //require 'PHPMailer/PHPMailer.php';
+    //require 'PHPMailer/SMTP.php';
+    require_once("PHPMailer/Exception.php");
+    require_once("PHPMailer/PHPMailer.php");
+    require_once("PHPMailer/SMTP.php");
+    //require_once("PHPMailerAutoload.php");
+
+    //require 'PHPMailer/autoload.php';
 
     class UserController{
 
@@ -55,8 +62,7 @@
                     $this->admin();
                 }else
                 {
-                    
-                    $this->user();
+                    $this->user($user);
                 }
             
             }else{
@@ -68,11 +74,12 @@
             require_once(VIEWS_PATH."registerCinema.php");
         }
 
-        public function user(){
-            $this->CargarCorreo();
+        public function user($user){
+            //$this->user();
+            //$this->CargarCorreo($user->getEmail());
             $movieList = $this->movieDAO->GetAllForShowingActivas();
             $genderList = $this->genderDAO->GetAll();
-            require_once(VIEWS_PATH."billboardMovie.php");
+            //require_once(VIEWS_PATH."billboardMovie.php");
 
 
         }
@@ -119,44 +126,61 @@
         }
 
 
-    public function CargarCorreo()
+    public function CargarCorreo($correo)
     {
-        $mail = new PHPMailer(true);
+        //$mail=null;
+        //var_dump($mail);
+        $mail = new PHPMailer();
+        //var_dump($mail);
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        //var_dump("hola");
+        $mail->SMTPDebug =SMTP::DEBUG_OFF;                    // Enable verbose debug output
+
         $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp1.gmail.com';   //buscar para hotmail.com or outlook.com                 // Set the SMTP server to send through
+        $mail->Host       = "ssl://smtp.gmail.com";   //buscar para hotmail.com or outlook.com                 // Set the SMTP server to send through
+        "ssl://smtp.gmail.com";
+        //'smtp1.gmail.com';
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'nicolasroldan31@gmail.com';                     // SMTP username
-        $mail->Password   = 'fernandamama28';                               // SMTP password
+        $mail->Password   = "fernandamama";                               // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
+//var_dump("hola");
         //Recipients
         $mail->setFrom('nicolasroldan31@gmail.com', 'Nicolas');
-        $mail->addAddress('nicolasroldan31@gmail.com', 'Nicolas');     // Add a recipient
+        $mail->addAddress("$correo", 'Nicolas');     // Add a recipient
         //$mail->addAddress('ellen@example.com');               // Name is optional
         //$mail->addReplyTo('info@example.com', 'Information');
         //$mail->addCC('cc@example.com');
         //$mail->addBCC('bcc@example.com');
-
+        //
+        //echo FRONT_ROOT.VIEWS_PATH."img\pngwing.com.png"
+        //
         // Attachments
-        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        $mail->addAttachment('Views\img\pngwing.com.png');         // Add attachments
         //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
         // Content
         $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->Subject = 'weeeeee';   //asunto
+        $mail->Body    = 'hola <b>xD</b>';
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+            )
+        );
         //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+        //var_dump($mail);
         $mail->send();
         echo 'Message has been sent';
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
+    //var_dump($mail);
     }
 }
 ?>
